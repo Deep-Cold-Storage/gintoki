47381a3eb002c6520725b52eae462a49afe4898c
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-06-06 17:44:50 +0200

    Add lockers model

---
+++ backend/models/lockers.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const point = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
+const schema = mongoose.Schema({
+  name: String,
+  key: String,
+  geo: {
+    type: point,
+    required: true,
+  },
+  slots: [{ name: String, pin: Number, occupied: Boolean, owners: [{ _userId: mongoose.Types.ObjectId }] }],
+  commands: [{ action: String, pin: Number }],
+});
+
+module.exports = mongoose.model('locker', schema);
