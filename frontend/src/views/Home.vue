47910ce7adb3752da73eacc08d695116493c8fd1
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-06-07 17:31:55 +0200

    Trigger workflow

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="mx-5 my-5 text-2xl font-bold text-primary">Hello World?</h1>
+  <h1 class="mx-5 my-5 text-2xl font-bold text-primary">Hello World</h1>
 </template>
 
 <script>
