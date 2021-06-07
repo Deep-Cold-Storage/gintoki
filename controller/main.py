5d36ebd7900ca09e3dbbc967fef5c5362465c8e0
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-06-07 19:33:02 +0200

    Add GPIO logic

--- controller/main.py
+++ controller/main.py
@@ -1,9 +1,22 @@
-import urequests
 import time
+import machine
+
+import urequests
 
 from config import Config
 
 config = Config()
+
+
+def open_door(pin):
+    pin = machine.Pin(pin)
+
+    pin.off()
+    time.sleep(1)
+    pin.on()
+    time.sleep(1)
+    pin.off()
+
 
 while True:
     response = urequests.get("https://gintoki.bednarski.dev/api/lockers/" + config.locker + "/commands", headers={"key": config.key})
@@ -11,4 +24,8 @@ while True:
     print(response.status_code)
     print(response.json())
 
-    time.sleep(5)
+    for command in response.json():
+        if command["action"] == "open":
+            open_door(command["pin"])
+
+    time.sleep(1)
