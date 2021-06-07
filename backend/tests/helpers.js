44f329608d74709bd63e36e08378a4dd3fd181c5
Galiantos <galiantos007@gmail.com>
2021-06-07 14:25:23 +0200

    Add items test

--- backend/tests/helpers.js
+++ backend/tests/helpers.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const users = require('../models/users');
+const lockers = require('../models/lockers');
 
 const getAuthToken = () => {
   let user = new users({ email: 'testing@gmail.com' });
@@ -8,6 +9,15 @@ const getAuthToken = () => {
   return jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1w' });
 };
 
+const getLocker = () => {
+  let locker = new lockers({ name: 'Locker', location: [21.973228,50.015645], slots: [{pin:21},{pin:21},{pin:21}]});
+  locker.save();
+
+  return locker;
+};
+
+
 module.exports = {
   getAuthToken,
+  getLocker
 };
