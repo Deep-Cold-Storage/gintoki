1f62337131a64f05c3a7087658001e0d6748831f
IAm226 <maciek.mokrzycki2@gmail.com>
2021-06-15 16:02:01 +0200

    Delete unused view and fix padding

--- frontend/src/components/TopNavigation.vue
+++ frontend/src/components/TopNavigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="static flex flex-row items-center justify-between px-16 py-2 md:py-3 border-b border-primary shadow-md bg-moderngrey bg-gradient-to-t from-gray">
-    <h1 class="py-5 font-medium cursor-pointer text-2xl" @click="$router.push('/').catch((err) => {})">📦</h1>
+  <header class="static flex flex-row items-center justify-between px-10 py-2 md:py-3 border-b border-primary shadow-md bg-moderngrey bg-gradient-to-t from-gray">
+    <h1 class="py-2 md:py-5 font-medium cursor-pointer text-2xl" @click="$router.push('/').catch((err) => {})">📦</h1>
 
     <nav class="flex items-center justify-center">
       <router-link v-if="logged && $route.name != 'Items'" class="lg:mx-auto text-sm font-medium text-white text-center rounded lg:max-w-max bg-primary focus:outline-none px-8 py-3" to="/items">Your items</router-link>
